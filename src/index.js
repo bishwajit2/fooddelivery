658e73c190a6fcbe3b0820df9cca00a793d4d5ad
Bishwajit <bishwajitdebnath9049.bd@gmail.com>
2022-08-08 12:19:35 +0600

    Some changes in package.json file

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import {StateProvider} from "./context/StateProvider";
 import {initialState} from "./context/initialState";
 import reducer from "./context/reducer";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Router basename="/fooddelivery">
+  <Router>
     <StateProvider initialState={initialState} reducer={reducer}>
       <App />
     </StateProvider>
